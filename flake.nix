3e9d6989025b92285480586a57968ead3cd15362
x-di <dimasimam75@gmail.com>
2024-05-22 03:23:08 +0700

    feat(flake): add home manager

--- flake.nix
+++ flake.nix
@@ -1,20 +1,22 @@
 {
+  description = "NixOS configuration";
   inputs = {
-    # NOTE: Replace "nixos-23.11" with that which is in system.stateVersion of
-    # configuration.nix. You can also use latter versions if you wish to
-    # upgrade.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    # NOTE: 'nixos' is the default hostname set by the installer
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      # NOTE: Change this to aarch64-linux if you are on ARM
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager
         {
-          nix = {
-            settings.experimental-features = [ "nix-command" "flakes" ];
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.xd = ./home.nix;
+          nix.settings = {
+            experimental-features = [ "nix-command" "flakes" ];
           };
         }
       ];
---
+++ home.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+let
+  name = "xd";
+  homeDirectory = "/home/xd";
+  stateVersion = "24.05";
+  userName = "x-di";
+  userEmail = "dimasimam75@gmail.com";
+  # gpg --list-secret-keys --keyid-format=long
+  signingkey = "11E66F143A6E166B";
+in
+{
+  home = {
+    username = "${name}";
+    homeDirectory = "${homeDirectory}";
+    stateVersion = "${stateVersion}";
+  };
+  nixpkgs.config.allowUnfree = true;
+  programs.git = {
+    enable = true;
+    userName = "${userName}";
+    userEmail = "${userEmail}";
+    signing = {
+      signByDefault = true;
+      key = "${signingkey}";
+    };
+  };
+}
