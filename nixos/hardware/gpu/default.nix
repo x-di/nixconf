30ec444ddfa502174bcd704cf5dc3acb1587ddf8
x-di <dimasimam75@gmail.com>
2024-05-24 01:19:07 +0700

    feat(hardware): Accelerated Video Playback

--- nixos/hardware/gpu/default.nix
+++ nixos/hardware/gpu/default.nix
@@ -1,12 +1,19 @@
 { pkgs, ... }:
 {
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
   hardware.opengl = {
     enable = true;
     driSupport32Bit = true;
-    # mesaPackage = pkgs.mesa_23;
-    # mesaPackage32 = pkgs.pkgsi686Linux.mesa_23;
     package = pkgs.mesa.drivers;
     package32 = pkgs.pkgsi686Linux.mesa.drivers;
-    extraPackages = [ pkgs.mesa.opencl ];
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+      mesa.opencl
+    ];
   };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
 }
