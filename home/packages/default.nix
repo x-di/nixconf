48a00a510d8ae5f8afd24bcec301e017df14f62c
x-di <dimasimam75@gmail.com>
2024-05-24 01:33:33 +0700

    feat(packages): add gstreamer

--- home/packages/default.nix
+++ home/packages/default.nix
@@ -7,6 +7,11 @@
       fastfetch
       glxinfo
       gnome.gnome-keyring
+      gst_all_1.gstreamer
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-plugins-ugly
+      gst_all_1.gst-vaapi
       haruna
       kdePackages.kdeplasma-addons
       libdbusmenu
