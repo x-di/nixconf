f0832487175a73e556125d26512571472868a996
x-di <dimasimam75@gmail.com>
2024-05-25 05:04:52 +0700

    feat(programs): add obs-studio

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -14,6 +14,7 @@
     ./micro
     ./mpv
     # ./nixvim
+    ./obs-studio
     ./starship
     ./yt-dlp
     ./zoxide
---
+++ home/programs/obs-studio/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  programs.obs-studio = {
+    enable = true;
+    # package = (pkgs.obs-studio.override { ffmpeg = pkgs.ffmpeg_7-full; });
+    plugins = with pkgs.obs-studio-plugins; [
+      # obs-multi-rtmp
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-move-transition
+      input-overlay
+      obs-vkcapture
+      obs-vaapi
+      obs-tuna
+      obs-transition-table
+      obs-text-pthread
+      obs-source-switcher
+      obs-pipewire-audio-capture
+      input-overlay
+    ];
+  };
+}
