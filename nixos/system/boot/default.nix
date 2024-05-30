a3b061ad9280fc8f1860417125c0d14b84568c9b
x-di <dimasimam75@gmail.com>
2024-05-30 16:26:22 +0700

    feat(boot): change kernel generic

--- nixos/system/boot/default.nix
+++ nixos/system/boot/default.nix
@@ -10,16 +10,16 @@
       };
     };
     # kernelPackages = pkgs.linuxPackages_latest;
-    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    # kernelPackages = pkgs.linuxPackages_xanmod_latest;
     # kernelPackages = pkgs.linuxPackages_zen;
-    kernel = {
-      sysctl = {
-        "vm.swappiness" = 180;
-        "vm.watermark_boost_factor" = 0;
-        "vm.watermark_scale_factor" = 125;
-        "vm.page-cluster" = 0;
-      };
-    };
+    # kernel = {
+      # sysctl = {
+        # "vm.swappiness" = 180;
+        # "vm.watermark_boost_factor" = 0;
+        # "vm.watermark_scale_factor" = 125;
+        # "vm.page-cluster" = 0;
+      # };
+    # };
     # kernelParams = [
     #   "i915.enable_fbc=1"
     #   "i915.enable_psr=2"
