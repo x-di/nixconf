a6b00abdd8c19f3f346d464717db5f75e93df904
x-di <dimasimam75@gmail.com>
2024-06-21 03:08:12 +0700

    feat(boot): change kernel xanmod latest

--- nixos/system/boot/default.nix
+++ nixos/system/boot/default.nix
@@ -10,8 +10,8 @@
       };
     };
     # kernelPackages = pkgs.linuxPackages_latest;
-    kernelPackages = pkgs.linuxPackages_xanmod;
-    # kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    # kernelPackages = pkgs.linuxPackages_xanmod;
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
     # kernelPackages = pkgs.linuxPackages_zen;
     # kernelPackages = pkgs.linuxPackages_lqx;
     # kernel = {
