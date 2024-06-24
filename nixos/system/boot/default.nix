8a6e30d7b3ba5323d748878fcf137812736187e9
x-di <dimasimam75@gmail.com>
2024-06-25 05:13:04 +0700

    feat(boot): add plymouth and enable silent boot

--- nixos/system/boot/default.nix
+++ nixos/system/boot/default.nix
@@ -15,16 +15,43 @@
     # kernelPackages = pkgs.linuxPackages_zen;
     # kernelPackages = pkgs.linuxPackages_lqx;
     # kernel = {
-      # sysctl = {
-        # "vm.swappiness" = 180;
-        # "vm.watermark_boost_factor" = 0;
-        # "vm.watermark_scale_factor" = 125;
-        # "vm.page-cluster" = 0;
-      # };
+    # sysctl = {
+    # "vm.swappiness" = 180;
+    # "vm.watermark_boost_factor" = 0;
+    # "vm.watermark_scale_factor" = 125;
+    # "vm.page-cluster" = 0;
+    # };
     # };
     # kernelParams = [
     #   "i915.enable_fbc=1"
     #   "i915.enable_psr=2"
     # ];
+    plymouth = {
+      enable = true;
+      # theme = "rings";
+      # themePackages = with pkgs; [
+      #   # By default we would install all themes
+      #   (adi1090x-plymouth-themes.override {
+      #     selected_themes = [ "rings" ];
+      #   })
+      # ];
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
   };
 }
