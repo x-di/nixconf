503b9bd51318da3a2490863fee9c9bd58930a757
x-di <dimasimam75@gmail.com>
2024-07-06 05:35:54 +0700

    fix(system): swapfile - 6GB

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -39,7 +39,7 @@
 
   swapDevices = [{
     device = "/var/lib/swapfile";
-    size = 4 * 1024;
+    size = 6 * 1024;
   }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
