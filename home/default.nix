5524cf7365927f33083340fdd346ec48c55d0113
x-di <dimasimam75@gmail.com>
2024-07-22 05:56:41 +0700

    fix(home): theme - catppuccin mocha mauve

--- home/default.nix
+++ home/default.nix
@@ -7,7 +7,7 @@ in
 {
   catppuccin = {
     enable = true;
-    accent = "lavender"; # Type: one of “blue”, “flamingo”, “green”, “lavender”, “maroon”, “mauve”, “peach”, “pink”, “red”, “rosewater”, “sapphire”, “sky”, “teal”, “yellow”
+    accent = "mauve"; # Type: one of “blue”, “flamingo”, “green”, “lavender”, “maroon”, “mauve”, “peach”, “pink”, “red”, “rosewater”, “sapphire”, “sky”, “teal”, “yellow”
     flavor = "mocha"; # Type: one of “latte”, “frappe”, “macchiato”, “mocha”
   };
   home = {
@@ -17,7 +17,7 @@ in
   };
   nixpkgs.config.allowUnfree = true;
   imports = [
-    (import ./langserver)
+    #(import ./langserver)
     (import ./packages)
     (import ./programs)
     (import ./services)
