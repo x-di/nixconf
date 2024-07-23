1bbda5b5886bdfd646f683bd8fb924259e598a02
x-di <dimasimam75@gmail.com>
2024-07-23 15:29:14 +0700

    fix(system): add nix settings

--- flake.nix
+++ flake.nix
@@ -34,7 +34,18 @@
           ];
           home-manager.users.dim = (import ./home inputs);
           nix.settings = {
+            keep-outputs = true;
+            keep-derivations = true;
             experimental-features = [ "nix-command" "flakes" ];
+            substituters = [
+              "https://cache.nixos.org/"
+              "https://cache.komunix.org/"
+              "https://nix-community.cachix.org"
+            ];
+            trusted-public-keys = [
+              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            ];
+            fallback = true;
           };
         }
       ];
