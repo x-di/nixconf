1888dfa3c7c8bbee93526d0759cbdc2f2b73be3e
x-di <dimasimam75@gmail.com>
2024-07-28 04:56:38 +0700

    fix(system): font - add font

--- nixos/system/fonts/default.nix
+++ nixos/system/fonts/default.nix
@@ -3,6 +3,6 @@
   fonts.packages = with pkgs; [
     carlito
     geist-font
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+    (nerdfonts.override { fonts = [ "FiraCode" "GeistMono" "JetBrainsMono" ]; })
   ];
 }
