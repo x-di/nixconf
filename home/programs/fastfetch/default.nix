2077d35733648341e37d79338f1e9096cda537ba
x-di <dimasimam75@gmail.com>
2024-08-03 05:22:16 +0700

    fix(programs): fastfetch - custom config

--- home/programs/fastfetch/default.nix
+++ home/programs/fastfetch/default.nix
@@ -1,156 +1,124 @@
 { ... }:
 {
-  programs = {
-    fastfetch = {
-      enable = true;
-      settings = {
-        logo = {
-          type = "none";
-          padding = {
-            right = 1;
-            top = 2;
-          };
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      # schema = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+      logo = {
+        type = "none";
+        padding = {
+          right = 1;
+          top = 2;
         };
-        display = {
-          size.binaryPrefix = "si";
-          color = "blue";
-          separator = "  ";
-        };
-        modules = [
-          "title"
-          "separator"
-          {
-            type = "os";
-            key = "SYSTEM";
-            keyColor = "yellow";
-            format = "{2}";
-          }
-          {
-            type = "os";
-            key = "├ ";
-            keyColor = "yellow";
-            format = "{6}{?6} {?}{10}{?10} {?}{8}{?8} {?}{11}";
-          }
-          {
-            type = "kernel";
-            key = "├ ";
-            keyColor = "yellow";
-          }
-          {
-            type = "packages";
-            key = "├ ";
-            keyColor = "yellow";
-          }
-          {
-            type = "shell";
-            key = "└ ";
-            keyColor = "yellow";
-          }
-          "break"
-          {
-            type = "wm";
-            key = "DESKTOP";
-            keyColor = "blue";
-          }
-          {
-            type = "lm";
-            key = "├ ";
-            keyColor = "blue";
-          }
-          {
-            type = "theme";
-            key = "├ ";
-            keyColor = "blue";
-          }
-          {
-            type = "icons";
-            key = "├ ";
-            keyColor = "blue";
-          }
-          {
-            type = "font";
-            key = "├ ";
-            keyColor = "blue";
-          }
-          {
-            type = "cursor";
-            key = "├ ";
-            keyColor = "blue";
-          }
-          {
-            type = "terminal";
-            key = "├ ";
-            keyColor = "blue";
-          }
-          {
-            type = "version";
-            key = "└ ";
-            keyColor = "blue";
-          }
-          "break"
-          {
-            type = "host";
-            key = "HARDWARE";
-            keyColor = "green";
-          }
-          {
-            type = "cpu";
-            key = "├ ";
-            keyColor = "green";
-          }
-          {
-            type = "gpu";
-            key = "├ ";
-            keyColor = "green";
-          }
-          {
-            type = "disk";
-            key = "├ ";
-            keyColor = "green";
-          }
-          {
-            type = "memory";
-            key = "├ ";
-            keyColor = "green";
-          }
-          {
-            type = "uptime";
-            key = "├ ";
-            keyColor = "green";
-          }
-          {
-            type = "display";
-            key = "├ ";
-            keyColor = "green";
-          }
-          {
-            type = "locale";
-            key = "├ ";
-            keyColor = "green";
-          }
-          {
-            type = "vulkan";
-            key = "├ ";
-            keyColor = "green";
-          }
-          {
-            type = "opengl";
-            key = "├ ";
-            keyColor = "green";
-          }
-          {
-            type = "opencl";
-            key = "├ ";
-            keyColor = "green";
-          }
-          {
-            type = "sound";
-            key = "└ ";
-            keyColor = "green";
-          }
-          "break"
-        ];
       };
+      display = {
+        separator = " ";
+      };
+      modules = [
+        {
+          type = "host";
+          key = "╭─󰌢";
+          keyColor = "green";
+        }
+        {
+          type = "cpu";
+          key = "├─󰻠";
+          keyColor = "green";
+        }
+        {
+          type = "gpu";
+          key = "├─󰍛";
+          keyColor = "green";
+        }
+        {
+          type = "disk";
+          key = "├─";
+          keyColor = "green";
+        }
+        {
+          type = "memory";
+          key = "├─󰑭";
+          keyColor = "green";
+        }
+        {
+          type = "swap";
+          key = "├─󰓡";
+          keyColor = "green";
+        }
+        {
+          type = "display";
+          key = "╰─󰍹";
+          keyColor = "green";
+        }
+
+        "break"
+
+        {
+          type = "shell";
+          key = "╭─";
+          keyColor = "yellow";
+        }
+        {
+          type = "terminal";
+          key = "├─";
+          keyColor = "yellow";
+        }
+        {
+          type = "terminalfont";
+          key = "├─";
+          keyColor = "yellow";
+        }
+        {
+          type = "de";
+          key = "├─";
+          keyColor = "yellow";
+        }
+        {
+          type = "wm";
+          key = "├─";
+          keyColor = "yellow";
+        }
+        {
+          type = "theme";
+          key = "├─󰉼";
+          keyColor = "yellow";
+        }
+        {
+          type = "icons";
+          key = "╰─󰀻";
+          keyColor = "yellow";
+        }
+
+        "break"
+
+        {
+          type = "title";
+          key = "╭─";
+          format = "{1}@{2}";
+          keyColor = "blue";
+        }
+        {
+          type = "os";
+          key = "├─"; # Just get your distro's logo off nerdfonts.com
+          keyColor = "blue";
+        }
+        {
+          type = "kernel";
+          key = "├─";
+          format = "{1} {2}";
+          keyColor = "blue";
+        }
+        {
+          type = "packages";
+          key = "├─󰏖";
+          keyColor = "blue";
+        }
+        {
+          type = "uptime";
+          key = "╰─󰅐";
+          keyColor = "blue";
+        }
+      ];
     };
   };
 }
