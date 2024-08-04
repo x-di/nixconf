5f4b63fa5ba433f8b2813a291231e700e43a6b88
x-di <dimasimam75@gmail.com>
2024-08-04 15:23:17 +0700

    fix(system): change kernel xanmod

--- nixos/system/boot/default.nix
+++ nixos/system/boot/default.nix
@@ -10,9 +10,9 @@
       };
     };
     # kernelPackages = pkgs.linuxPackages_latest;
-    # kernelPackages = pkgs.linuxPackages_xanmod;
+    kernelPackages = pkgs.linuxPackages_xanmod;
     # kernelPackages = pkgs.linuxPackages_xanmod_latest;
-    kernelPackages = pkgs.linuxPackages_zen;
+    # kernelPackages = pkgs.linuxPackages_zen;
     # kernelPackages = pkgs.linuxPackages_lqx;
     kernel = {
       sysctl = {
