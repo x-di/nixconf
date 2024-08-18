6b086b193b529d96b9d2a13037ba46ff5baf94c2
x-di <dimasimam75@gmail.com>
2024-08-18 07:48:16 +0700

    fix(services): DNSCrypt - add Anonymized DNS

--- nixos/services/dnscrypt/default.nix
+++ nixos/services/dnscrypt/default.nix
@@ -5,6 +5,15 @@
       settings = {
         ipv6_servers = true;
         require_dnssec = true;
+        anonymized_dns = {
+          routes = [
+            {
+              server_name = "*";
+              via = [ "anon-tiarap" "anon-cs-singapore" "dnscry.pt-anon-singapore02-ipv4" "dnscry.pt-anon-singapore02-ipv6" ];
+            }
+          ];
+          skip_incompatible = true;
+        };
       };
     };
   };
