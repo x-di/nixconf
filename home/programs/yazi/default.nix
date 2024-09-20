878200c024e70033120c36ca9e7416f0f1a79716
x-di <dimasimam75@gmail.com>
2024-09-21 05:22:38 +0700

    feat(programs): add yazi

---
+++ home/programs/yazi/default.nix
@@ -0,0 +1,21 @@
+{ ... }:
+{
+  programs = {
+    yazi = {
+      enable = true;
+      settings = {
+        log = {
+          enabled = false;
+        };
+        manager = {
+          ratio = [ 1 5 2 ];
+          show_hidden = false;
+          sort_by = "alphabetical";
+          linemode = "size";
+          sort_dir_first = true;
+          sort_reverse = false;
+        };
+      };
+    };
+  };
+}
