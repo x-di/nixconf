b9067bc9e28c39883539a2d381429b8afdd45b67
x-di <dimasimam75@gmail.com>
2024-09-21 05:22:58 +0700

    feat(programs): add zellij

---
+++ home/programs/zellij/default.nix
@@ -0,0 +1,28 @@
+{ ... }:
+{
+  programs = {
+    zellij = {
+      enable = true;
+      enableFishIntegration = true;
+      settings = {
+        copy_command = "wl-copy";
+        default_layout = "compact";
+        default_shell = "fish";
+        pane_frames = false;
+        simplified_ui = true;
+        plugins = {
+          tab-bar.path = "tab-bar";
+          status-bar.path = "status-bar";
+          strider.path = "strider";
+          compact-bar.path = "compact-bar";
+        };
+        ui = {
+          pane_frames = {
+            hide_session_name = true;
+            rounded_corners = false;
+          };
+        };
+      };
+    };
+  };
+}
