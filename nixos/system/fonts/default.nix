fe4e6148bef3a69dae4fc4711873d2b5179cf240
x-di <dimasimam75@gmail.com>
2024-10-08 06:54:56 +0700

    fix(system): font - OpenMoji Color for the default emoji font

--- nixos/system/fonts/default.nix
+++ nixos/system/fonts/default.nix
@@ -1,9 +1,23 @@
 { pkgs, ... }:
 {
-  fonts.packages = with pkgs; [
-    carlito
-    geist-font
-    liberation_ttf
-    (nerdfonts.override { fonts = [ "FiraCode" "GeistMono" "JetBrainsMono" ]; })
-  ];
+  fonts = {
+    packages = with pkgs; [
+      carlito
+      geist-font
+      liberation_ttf
+      openmoji-color
+      (nerdfonts.override {
+        fonts = [
+          "FiraCode"
+          "GeistMono"
+          "JetBrainsMono"
+        ];
+      })
+    ];
+    fontconfig = {
+      defaultFonts = {
+        emoji = [ "OpenMoji Color" ];
+      };
+    };
+  };
 }
