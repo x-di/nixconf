da1dfa232adefce15d2e019339f85821d0db80b4
x-di <dimasimam75@gmail.com>
2024-11-08 15:00:23 +0700

    chore(flake): change nixpkgs url

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   description = "NixOS configuration";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     catppuccin.url = "github:catppuccin/nix";
