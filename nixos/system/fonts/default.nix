afe1e8045a1717225dc0993b1d65632638c77ba7
x-di <dimasimam75@gmail.com>
2024-11-30 18:40:16 +0700

    fix(system): fonts - nerd-fonts

--- nixos/system/fonts/default.nix
+++ nixos/system/fonts/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   fonts = {
-    packages = with pkgs; [
+    packages = (with pkgs; [
       carlito
       geist-font
       # joypixels
@@ -11,14 +11,13 @@
       noto-fonts-cjk-serif
       noto-fonts-color-emoji
       # openmoji-color
-      (nerdfonts.override {
-        fonts = [
-          "FiraCode"
-          "GeistMono"
-          "JetBrainsMono"
-        ];
-      })
-    ];
+    ])
+    ++ (with pkgs.nerd-fonts; [
+      fira-code
+      geist-mono
+      jetbrains-mono
+      zed-mono
+    ]);
     fontconfig = {
       defaultFonts = {
         # emoji = [ "OpenMoji Color" ];
