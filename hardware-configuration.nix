177fb21da5b06de4edaf82276466ca0e619d0c55
x-di <dimasimam75@gmail.com>
2025-01-16 08:27:58 +0700

    feat(system): add swapfile

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -52,7 +52,13 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/mnt/data/sys/swapfile";
+      size = 12 * 1024;
+      randomEncryption.enable = true;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
