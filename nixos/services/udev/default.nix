ab58dec0251a31dad3e38619955c6b32f7534d01
x-di <dimasimam75@gmail.com>
2025-02-12 03:58:25 +0700

    feat(system): tweak from cachyos and kernel cachyos

--- nixos/services/udev/default.nix
+++ nixos/services/udev/default.nix
@@ -1,7 +1,20 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  services.udev.packages = [ pkgs.gnome-settings-daemon ];
-  services.udev.extraRules = ''
-    KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
-  '';
+  services.udev = {
+    enable = true;
+    extraRules = ''
+      ACTION=="add", SUBSYSTEM=="scsi_host", KERNEL=="host*", \
+          ATTR{link_power_management_policy}=="*", \
+          ATTR{link_power_management_policy}="max_performance"
+
+      ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", \
+          ATTR{queue/scheduler}="bfq"
+
+      ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", \
+          ATTR{queue/scheduler}="mq-deadline"
+
+      ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", \
+          ATTR{queue/scheduler}="none"
+    '';
+  };
 }
