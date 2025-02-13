9f68a31823080faf4d74a8990d89366369b6dd33
x-di <dimasimam75@gmail.com>
2025-02-13 20:14:07 +0700

    chore(system): driver - mesa_git

--- nixos/hardware/default.nix
+++ nixos/hardware/default.nix
@@ -1,10 +1,10 @@
 { ... }:
 {
   imports = [
-    # ./chaotic
+    ./chaotic
     # ./cpu
     # ./filesystem
-    ./gpu
+    # ./gpu
     # ./i2c
   ];
 }
--- nixos/system/environment/default.nix
+++ nixos/system/environment/default.nix
@@ -6,6 +6,7 @@
       # MESA_VK_DEVICE_SELECT = "1002:6900";
       GTK_USE_PORTAL = "1";
       MOZ_USE_XINPUT2 = "1";
+      LIBVA_DRIVER_NAME = "iHD";
     };
     etc."chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json".source =
       "${pkgs.kdePackages.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
