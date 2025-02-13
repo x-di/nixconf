15278058d19732fcda197b0b723d12d297acbeaa
x-di <dimasimam75@gmail.com>
2025-02-14 05:45:45 +0700

    chore(services): scx - change scheduler to scx_rusty

--- nixos/services/scx/default.nix
+++ nixos/services/scx/default.nix
@@ -3,7 +3,7 @@
   services.scx = {
     enable = true;
     package = pkgs.scx.rustscheds;
-    scheduler = "scx_lavd";
+    scheduler = "scx_rusty";
     extraArgs = [
       # "--autopower" # scx_lavd
       # "--verbose"
