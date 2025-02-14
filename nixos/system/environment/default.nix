ed3caef0268854d5601d609e4008a4fa1ec864b6
x-di <dimasimam75@gmail.com>
2025-02-15 05:10:01 +0700

    chore(system): environment - add gst plugin

--- nixos/system/environment/default.nix
+++ nixos/system/environment/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   environment = {
     sessionVariables = {
@@ -7,6 +7,11 @@
       GTK_USE_PORTAL = "1";
       MOZ_USE_XINPUT2 = "1";
       LIBVA_DRIVER_NAME = "iHD";
+      GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+        pkgs.gst_all_1.gst-plugins-base
+        pkgs.gst_all_1.gst-plugins-good
+        pkgs.gst_all_1.gst-libav
+      ];
     };
     etc."chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json".source =
       "${pkgs.kdePackages.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
