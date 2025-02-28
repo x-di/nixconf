d2789e10a2bc0396e71966bfcb133d1b839ab6c3
x-di <dimasimam75@gmail.com>
2025-02-28 12:54:53 +0700

    fix(services): btrbk config

--- nixos/services/btrbk/default.nix
+++ nixos/services/btrbk/default.nix
@@ -10,31 +10,24 @@
           snapshot_preserve = "48h 14d 4w";
           volume = {
             "/home" = {
-              snapshot_dir = "/mnt/data/.snapshots/home";
-              subvolume = "home";
+              snapshot_dir = ".snapshots";
+              subvolume = "/home";
             };
             "/mnt/data/personal" = {
-              snapshot_dir = "/mnt/data/.snapshots/personal";
-              subvolume = "personal";
+              snapshot_dir = ".snapshots";
+              subvolume = "/mnt/data/personal";
             };
             "/mnt/data/work" = {
-              snapshot_dir = "/mnt/data/.snapshots/work";
-              subvolume = "work";
+              snapshot_dir = ".snapshots";
+              subvolume = "/mnt/data/work";
             };
             "/mnt/data/misc" = {
-              snapshot_dir = "/mnt/data/.snapshots/misc";
-              subvolume = "misc";
+              snapshot_dir = ".snapshots";
+              subvolume = "/mnt/data/misc";
             };
           };
         };
       };
     };
   };
-  systemd.tmpfiles.rules = [
-    "d /mnt/data/.snapshots 0755 root root"
-    "d /mnt/data/.snapshots/home 0755 root root"
-    "d /mnt/data/.snapshots/personal 0755 root root"
-    "d /mnt/data/.snapshots/work 0755 root root"
-    "d /mnt/data/.snapshots/misc 0755 root root"
-  ];
 }
