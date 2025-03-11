b9b4888543089d4a31f560f91f6d2ae1bd718846
x-di <dimasimam75@gmail.com>
2025-03-11 19:12:50 +0700

    chore(system): boot - change kernel xanmod latest

--- nixos/system/boot/default.nix
+++ nixos/system/boot/default.nix
@@ -13,10 +13,10 @@
         canTouchEfiVariables = true;
       };
     };
-    kernelPackages = pkgs.linuxPackages_latest;
+    # kernelPackages = pkgs.linuxPackages_latest;
     # kernelPackages = pkgs.linuxPackages-rt_latest;
     # kernelPackages = pkgs.linuxPackages_xanmod;
-    # kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
     # kernelPackages = pkgs.linuxPackages_zen;
     # kernelPackages = pkgs.linuxPackages_lqx;
     # kernelPackages = pkgs.linuxPackages_cachyos;
