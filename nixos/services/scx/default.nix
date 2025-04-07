de832cb11c778a1c994dd6042e3719b1e089a390
x-di <dimasimam75@gmail.com>
2025-04-08 06:13:06 +0700

    chore(services): scx - change scheduler to scx_lavd

--- nixos/services/scx/default.nix
+++ nixos/services/scx/default.nix
@@ -3,7 +3,7 @@
   services.scx = {
     enable = true;
     package = pkgs.scx.rustscheds;
-    scheduler = "scx_flash";
+    scheduler = "scx_lavd";
     extraArgs = [
       # "--autopower" # scx_lavd
       # "--verbose"
