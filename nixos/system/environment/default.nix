7adf3ac4ebf0e2e0004046619174f025fa7d7841
x-di <dimasimam75@gmail.com>
2025-04-22 05:32:06 +0700

    chore(system): Enable MESA_VK_DEVICE_SELECT for AMD GPU

--- nixos/system/environment/default.nix
+++ nixos/system/environment/default.nix
@@ -3,7 +3,7 @@
   environment = {
     sessionVariables = {
       NIXOS_OZONE_WL = "1";
-      # MESA_VK_DEVICE_SELECT = "1002:6900";
+      MESA_VK_DEVICE_SELECT = "1002:6900";
       GTK_USE_PORTAL = "1";
       MOZ_USE_XINPUT2 = "1";
       LIBVA_DRIVER_NAME = "iHD";
