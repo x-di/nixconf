af38e30d958f937bb49a43b57de874683bf12445
x-di <dimasimam75@gmail.com>
2025-04-22 12:18:39 +0700

    chore(services): scx - Switch SCX scheduler to scx_flash and update extraArgs

--- nixos/services/scx/default.nix
+++ nixos/services/scx/default.nix
@@ -3,8 +3,9 @@
   services.scx = {
     enable = true;
     package = pkgs.scx.rustscheds;
-    scheduler = "scx_bpfland";
+    scheduler = "scx_flash";
     extraArgs = [
+      "-k" # scx_flash | scx_bpfland
       # "--autopower" # scx_lavd
       # "--verbose"
     ];
