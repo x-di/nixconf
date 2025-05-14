ae2dbe713511b355026101006d5c0899ab086f18
x-di <dimasimam75@gmail.com>
2025-05-14 12:55:12 +0700

    chore(services): scx - Switch to scx_bpfland scheduler
    Adjust extra arguments to match the requirements of the scx_bpfland
    scheduler.

--- nixos/services/scx/default.nix
+++ nixos/services/scx/default.nix
@@ -3,23 +3,29 @@
   services.scx = {
     enable = true;
     package = pkgs.scx_git.rustscheds;
-    scheduler = "scx_rusty";
+    scheduler = "scx_bpfland";
     extraArgs = [
-      # "-f" # scx_bpfland | scx_rusty
+      "-s"
+      "5000"
+      "-S"
+      "300"
+      "-l"
+      "5000"
+      "-f" # scx_bpfland | scx_rusty
       # "-n" # scx_bpfland
-      # "-p" # scx_bpfland
-      # "-k" # scx_flash | scx_bpfland | scx_rusty
+      "-p" # scx_bpfland
+      "-k" # scx_flash | scx_bpfland | scx_rusty
       # "--autopower" # scx_lavd
       # "--verbose"
-      "-g"
-      "1"
-      "-D"
-      "80.0"
-      "-K"
-      "100.0"
-      "-b"
-      "-k"
-      "-f"
+      # "-g"
+      # "1"
+      # "-D"
+      # "80.0"
+      # "-K"
+      # "100.0"
+      # "-b"
+      # "-k"
+      # "-f"
     ];
   };
 }
