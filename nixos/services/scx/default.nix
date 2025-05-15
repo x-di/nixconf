6b6d6d0f4517e1cbf06bf0468a9f9cb9802b75ed
x-di <dimasimam75@gmail.com>
2025-05-16 06:52:46 +0700

    chore(services): scx - Switch SCX scheduler to scx_lavd and update extraArgs

--- nixos/services/scx/default.nix
+++ nixos/services/scx/default.nix
@@ -3,19 +3,19 @@
   services.scx = {
     enable = true;
     package = pkgs.scx_git.rustscheds;
-    scheduler = "scx_bpfland";
+    scheduler = "scx_lavd";
     extraArgs = [
-      "-s"
-      "5000"
-      "-S"
-      "300"
-      "-l"
-      "5000"
-      "-f" # scx_bpfland | scx_rusty
-      # "-n" # scx_bpfland
-      "-p" # scx_bpfland
-      "-k" # scx_flash | scx_bpfland | scx_rusty
-      # "--autopower" # scx_lavd
+      # "-s"
+      # "5000"
+      # "-S"
+      # "300"
+      # "-l"
+      # "5000"
+      # "-f" # scx_bpfland | scx_rusty
+      # # "-n" # scx_bpfland
+      # "-p" # scx_bpfland
+      # "-k" # scx_flash | scx_bpfland | scx_rusty
+      "--autopower" # scx_lavd
       # "--verbose"
       # "-g"
       # "1"
