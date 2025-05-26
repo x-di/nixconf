85b78ca85082dd3910747c9fcb6fd768eba9a250
x-di <dimasimam75@gmail.com>
2025-05-27 03:26:30 +0700

    chore(services): scx - Switch scx scheduler from scx_flash to scx_lavd
    Replace scx_flash-specific args with scx_lavd autopilot and
    no-freq-scaling options. Remove all commented configuration options.

--- nixos/services/scx/default.nix
+++ nixos/services/scx/default.nix
@@ -3,29 +3,10 @@
   services.scx = {
     enable = true;
     package = pkgs.scx_git.rustscheds;
-    scheduler = "scx_flash";
+    scheduler = "scx_lavd";
     extraArgs = [
-      # "-s"
-      # "5000"
-      # "-S"
-      # "300"
-      # "-l"
-      # "5000"
-      # "-f" # scx_bpfland | scx_rusty
-      # # "-n" # scx_bpfland
-      # "-p" # scx_bpfland
-      "-k" # scx_flash | scx_bpfland | scx_rusty
-      # "--autopower" # scx_lavd
-      # "--verbose"
-      # "-g"
-      # "1"
-      # "-D"
-      # "80.0"
-      # "-K"
-      # "100.0"
-      # "-b"
-      # "-k"
-      # "-f"
+      "--autopilot"
+      "--no-freq-scaling"
     ];
   };
 }
