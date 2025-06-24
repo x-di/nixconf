3bdfab52289eddd526d17137757855833efc7a26
x-di <dimasimam75@gmail.com>
2025-06-25 05:44:10 +0700

    chore(services): Add NextDNS servers to systemd-resolved config
    Use NextDNS servers in addition to the existing Cloudflare servers for
    DNS resolution, providing redundancy and potentially improved
    performance.

--- nixos/services/systemd-resolved/default.nix
+++ nixos/services/systemd-resolved/default.nix
@@ -14,6 +14,10 @@
       "8.8.8.8"
     ];
     extraConfig = ''
+      DNS=45.90.28.0#d32d3f.dns.nextdns.io
+      DNS=2a07:a8c0::#d32d3f.dns.nextdns.io
+      DNS=45.90.30.0#d32d3f.dns.nextdns.io
+      DNS=2a07:a8c1::#d32d3f.dns.nextdns.io
       DNS=1.1.1.1#one.one.one.one
     '';
   };
