5d30f3c30b20a097c4321251930187a668b2bc8e
x-di <dimasimam75@gmail.com>
2025-12-15 09:47:24 +0700

    chore(system): Enable v4l2loopback kernel module and switch to latest kernel
    - Uncomment v4l2loopback in boot.kernelModules
    - Uncomment boot.extraModprobeConfig for v4l2loopback device setup
    - Switch from linuxPackages_zen to linuxPackages_latest

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -25,14 +25,14 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [
     "kvm-intel"
-    # "v4l2loopback"
+    "v4l2loopback"
   ];
   # boot.extraModulePackages = with config.boot.kernelPackages; [
   #   v4l2loopback
   # ];
-  # boot.extraModprobeConfig = ''
-  #   options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-  # '';
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/a9152a2b-054c-498c-a016-27720aae387e";
