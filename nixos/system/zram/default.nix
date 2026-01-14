f72546e2da3e5efecc91beb347398fb8d3bd85bd
x-di <dimasimam75@gmail.com>
2026-01-14 19:03:54 +0700

    chore(system): Increase zram memoryPercent to 200%
    This change doubles the allocated memory for zram swap devices. This
    should provide more breathing room for systems experiencing high memory
    pressure.

--- nixos/system/zram/default.nix
+++ nixos/system/zram/default.nix
@@ -3,7 +3,7 @@
   zramSwap = {
     enable = true;
     algorithm = "zstd"; # zstd lz4 lzo
-    memoryPercent = 100; # % of total memory
+    memoryPercent = 200; # % of total memory
     # swapDevices = 4; # number of devices
   };
 }
