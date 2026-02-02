87b75a231cc11904feed8da8cf1cf8b83701f6b9
x-di <dimasimam75@gmail.com>
2026-02-02 07:16:15 +0700

    chore(programs): Configure Alacritty colors to Gruvbox dark
    Uncomment the Alacritty color configuration and change the primary
    background to '#202020'.

--- home/programs/alacritty/default.nix
+++ home/programs/alacritty/default.nix
@@ -51,35 +51,35 @@
           blinking = "Always";
         };
       };
-      # colors = {
-      #   primary = {
-      #     # hard contrast background = = '#1d2021'
-      #     # default background = = '#282828'
-      #     # soft contrast background = = '#32302f'
-      #     background = "#282828";
-      #     foreground = "#ebdbb2";
-      #   };
-      #   normal = {
-      #     black = "#282828";
-      #     red = "#cc241d";
-      #     green = "#98971a";
-      #     yellow = "#d79921";
-      #     blue = "#458588";
-      #     magenta = "#b16286";
-      #     cyan = "#689d6a";
-      #     white = "#a89984";
-      #   };
-      #   bright = {
-      #     black = "#928374";
-      #     red = "#fb4934";
-      #     green = "#b8bb26";
-      #     yellow = "#fabd2f";
-      #     blue = "#83a598";
-      #     magenta = "#d3869b";
-      #     cyan = "#8ec07c";
-      #     white = "#ebdbb2";
-      #   };
-      # };
+      colors = {
+        primary = {
+          # hard contrast background = = '#1d2021'
+          # default background = = '#282828'
+          # soft contrast background = = '#32302f'
+          background = "#202020";
+          foreground = "#ebdbb2";
+        };
+        normal = {
+          black = "#282828";
+          red = "#cc241d";
+          green = "#98971a";
+          yellow = "#d79921";
+          blue = "#458588";
+          magenta = "#b16286";
+          cyan = "#689d6a";
+          white = "#a89984";
+        };
+        bright = {
+          black = "#928374";
+          red = "#fb4934";
+          green = "#b8bb26";
+          yellow = "#fabd2f";
+          blue = "#83a598";
+          magenta = "#d3869b";
+          cyan = "#8ec07c";
+          white = "#ebdbb2";
+        };
+      };
     };
   };
 }
